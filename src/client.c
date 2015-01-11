bc67291c6039de90e5415b74c5c8d358a079371b
LeCroissant <nico22330@gmail.com>
2015-01-11 17:24:28 +0100

    Initial commit (client/server)

---
+++ src/client.c
@@ -0,0 +1,97 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+
+#define SUCCESS 0
+#define ERROR 1
+#define SERVER_PORT 1500
+#define MAX_MSG 100
+
+int main(int argc, char * argv[])
+{
+
+    int sd, rc, i;
+    struct sockaddr_in localAddr, servAddr;
+    struct hostent * h;
+
+    //Verification du nombre d'arguments
+    if(argc < 3)
+    {
+        printf("Usage : %s <server> <data1> <data2> ... <dataN>\n", argv[0]);
+        exit(ERROR);
+    }
+
+    h = gethostbyname(argv[1]);
+    if(h == NULL)
+    {
+        printf("%s : Uncknow host '%s'", argv[0], argv[1]);
+        exit(ERROR);
+    }
+
+    //Initialisation des champs de la structure servAddr
+    servAddr.sin_family = h->h_addrtype;
+    memcpy((char *)&servAddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
+    servAddr.sin_port = htons(SERVER_PORT);
+
+    //Creation de la socket
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sd < 0)
+    {
+        perror("Cannot open socket : ");
+        exit(ERROR);
+    }
+
+    //Initialisation des champs de la structure localAddr
+    localAddr.sin_family = AF_INET;
+    localAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    localAddr.sin_port = htons(0);
+
+    //Bind
+    rc = bind(sd, (struct sockaddr *) &localAddr, sizeof(localAddr));
+    if(rc < 0)
+    {
+        printf("%s : cannot bind port TCP %u\n", argv[0], SERVER_PORT);
+        perror("Error : ");
+        exit(ERROR);
+    }
+
+    //Connexion au serveur
+    rc = connect(sd, (struct sockaddr *) &servAddr, sizeof(servAddr));
+    if(rc < 0)
+    {
+        perror("Cannot connect : ");
+        exit(ERROR);
+    }
+
+
+    rc = send(sd, argv[2], strlen(argv[2])+1, 0);
+    if(rc < 0)
+    {
+        printf("Cannot send data : ");
+        close(sd);
+        exit(ERROR);
+    }
+
+    rc = send(sd, argv[3], strlen(argv[3])+1, 0);
+    if(rc < 0)
+    {
+        printf("Cannot send data : ");
+        close(sd);
+        exit(ERROR);
+    }
+
+    printf("%s : data %u send (%s)\n", argv[0], i-1, argv[2]);
+		printf("%s : data %u send (%s)\n", argv[0], i-1, argv[3]);
+
+    return(SUCCESS);
+}
+
+
---
+++ src/commun.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <netdb.h>
+
+
+#define LOCALHOST "127.0.0.1"
+#define PORT 1500
+
