02b4f5796b46d873ab89fbb403fbfc4c41f486a6
LeCroissant <nico22330@gmail.com>
2015-01-13 23:54:51 +0100

    Pas le droit d'initialiser variable dans struct

--- src/salon.h
+++ src/salon.h
@@ -4,7 +4,7 @@
 
 typedef struct
 {
-	int liste_joueur[2] = {0}; 
-	int grille[TAILLE_LIGNE][TAILLE_COLONNE] = {0};
+	int liste_joueur[2]; 
+	int grille[TAILLE_LIGNE][TAILLE_COLONNE];
 	
 } Salon_t;
