d9887af5c06e38f2e51884be85d53672e28a98ac
LeCroissant <nico22330@gmail.com>
2015-01-20 21:38:13 +0100

    Nommage enum couleur

--- src/grille.h
+++ src/grille.h
@@ -2,14 +2,14 @@
 #define TAILLE_LIGNE 7
 #define TAILLE_COLONNE 6
 
-enum couleurs {BLANC = 0, ROUGE = 1, JAUNE = 2};
+typedef enum  Couleur {BLANC, ROUGE, JAUNE} Couleur;
 
 
 
 
-void afficherGrille(int matrice[TAILLE_LIGNE][TAILLE_COLONNE]);
-int is_win(int grille[TAILLE_LIGNE][TAILLE_COLONNE]);
-int placerJeton(int position, int couleur, int matrice[TAILLE_LIGNE][TAILLE_COLONNE]);
+void afficherGrille(Couleur matrice[TAILLE_LIGNE][TAILLE_COLONNE]);
+int is_win(Couleur grille[TAILLE_LIGNE][TAILLE_COLONNE]);
+int placerJeton(int position, Couleur couleur, Couleur matrice[TAILLE_LIGNE][TAILLE_COLONNE]);
 
 
 
