7ea5b88cbb5267755f1b9379c948a36c9ca1f67b
LeCroissant <nico22330@gmail.com>
2015-01-21 01:27:19 +0100

    Client jouable

--- src/mainClient.c
+++ src/mainClient.c
@@ -23,26 +23,54 @@ int main()
 	printf("Veuillez choisir un numéro de salon ou tapez 0 pour quitter\n");
 	int choix;
 	scanf("%d", &choix);
-	
+	//Possibilité de faire ça avec un for?
 	switch(choix)
 	{
 
 		
 			case 1:
-				//Je ne sais pas encore comment on va faire pour determiner la variable joueur
-				//rejoindre_salon(sd, &salon[i_case]);
-				//printf("Connexion au salon n°%d\n", i_case);
+				rejoindre_salon(sd, 1);
+				printf("Connexion au salon n°1\n");
 				break;
+			
+			case 2:
+				rejoindre_salon(sd, 2);
+				printf("Connexion au salon n°2\n");
+				break;	
+				
+			case 3:
+				rejoindre_salon(sd, 1);
+				printf("Connexion au salon n°1\n");
+				break;
+			
+			case 4:
+				rejoindre_salon(sd, 2);
+				printf("Connexion au salon n°2\n");
+				break;	
+					
 			case 0:
 				se_deconnecter(sd);
 				break;
+				
 			default:
 				printf("Choix inconnu\n");
 				break;
-		//}
 			
 			
 	}
+	
+	//Tant que la partie n'est pas finie
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		//Une fois que le joueur est connecté, on lui affiche la grille pour chaque fois ou un jeton est placé
+		int position;
+		printf("Position du jeton à placer\n");
+		scanf("%d", &position);
+		placer_jeton(sd, position);
+	}
+	
+	
 	return 0;
 }
 
