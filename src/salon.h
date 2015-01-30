b1da06d9ef3fb3ece98942d1e513424266e2f191
LeCroissant <nico22330@gmail.com>
2015-01-30 18:18:10 +0100

    Un salon contient un thread

--- src/salon.h
+++ src/salon.h
@@ -1,5 +1,6 @@
 #include "grille.h"
 #include <stdio.h>
+#include <pthread.h>
 
 /* Un salon contient une grille, et 2 joueurs */
 
@@ -8,12 +9,13 @@
 
 typedef struct
 {
+	int id;
 	int liste_joueur[2]; 
     Couleur joueur_courant;
 	Couleur grille[TAILLE_LIGNE][TAILLE_COLONNE];
 	int nb_joueurs;
+	pthread_t thread_salon;
 } Salon_t;
-
 
 Couleur ajouter_joueur(Salon_t * salon, int joueur);
 void afficher_infos_salon(Salon_t salon);
